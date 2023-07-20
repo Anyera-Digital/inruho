fa38181638bc8ad136fabe3811ee6196278ebb03
Buldakov <abuldakov10@gmail.com>
2023-07-20 15:58:05 +0400

    Add subscribe form, social block

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+/*** Change radio ***/
+$('.radio__input').on('click', function () {
+  $('.radio').removeClass('btn--bg-radio');
+  $(this).closest('.radio').addClass('btn--bg-radio')
+});
 
+/*** Subscribe ***/
+$('.js-subscribe').on('submit', function (e) {
+  e.preventDefault();
+
+  console.log('submit subscribe form');
+});
