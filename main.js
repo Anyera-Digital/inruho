7505b00d303a0d918bc25170f845b7cc2c1fb0a3
Buldakov <abuldakov10@gmail.com>
2023-07-21 12:21:26 +0400

    Add menu

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*** Change radio ***/
 $('.radio__input').on('click', function () {
   $('.radio').removeClass('btn--bg-radio');
-  $(this).closest('.radio').addClass('btn--bg-radio')
+  $(this).closest('.radio').addClass('btn--bg-radio');
 });
 
 /*** Subscribe ***/
@@ -9,4 +9,17 @@ $('.js-subscribe').on('submit', function (e) {
   e.preventDefault();
 
   console.log('submit subscribe form');
+});
+
+/*** Menu ***/
+// Open menu
+$('.js-company-details-menu').on('click', function (e) {
+  e.preventDefault();
+
+  $('body').addClass('no-scroll menu-open');
+});
+
+// Close menu
+$('.js-close-menu').on('click', function () {
+  $('body').removeClass('no-scroll menu-open');
 });
