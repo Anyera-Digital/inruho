6d6fa843e59755f8e091c8c48f29f5b79c96c8f8
Buldakov <abuldakov10@gmail.com>
2023-07-21 13:53:42 +0400

    Add timer

--- main.js
+++ main.js
@@ -23,3 +23,37 @@ $('.js-company-details-menu').on('click', function (e) {
 $('.js-close-menu').on('click', function () {
   $('body').removeClass('no-scroll menu-open');
 });
+
+/*** Timer ***/
+const endDate = new Date(2023,11,1,0,0); // end date
+
+const timeLeft = () => {
+  const date = new Date(); // current date
+  let secondsLeft = (endDate - date) / 1000; // seconds left
+
+  if (secondsLeft > 0) {
+    let minutesLeft = secondsLeft / 60; // minutes left
+    let hoursLeft = minutesLeft / 60; // hours left
+    let daysLeft = hoursLeft / 24; // days left
+
+    hoursLeft = (daysLeft - Math.floor(daysLeft)) * 24;
+    minutesLeft = (hoursLeft - Math.floor(hoursLeft)) * 60;
+    secondsLeft = (minutesLeft - Math.floor(minutesLeft)) * 60;
+
+    $('.js-timer .days').html(Math.floor(daysLeft));
+    $('.js-timer .hours').html((Math.floor(hoursLeft) < 10) ? `0${Math.floor(hoursLeft)}` : Math.floor(hoursLeft));
+    $('.js-timer .minutes').html((Math.floor(minutesLeft) < 10) ? `0${Math.floor(minutesLeft)}` : Math.floor(minutesLeft));
+    $('.js-timer .seconds').html((Math.floor(secondsLeft) < 10) ? `0${Math.floor(secondsLeft)}` : Math.floor(secondsLeft));
+  }
+  else {
+    return false
+  }
+
+  timer();
+}
+
+const timer = () => {
+  setTimeout(timeLeft, 1000);
+}
+
+timeLeft();
